88675bef7bbcfd8936af71baef920c7d76015a15
Filippo Ledda <filippo@metacell.us>
2019-02-14 16:05:01 +0100

    Linting fixes

--- App.js
+++ App.js
@@ -11,35 +11,36 @@ import PythonConsole from 'geppetto-client/js/components/interface/pythonConsole
 
 // list of props here --> https://github.com/mui-org/material-ui/blob/master/src/styles/baseThemes/lightBaseTheme.js
 const customTheme = {
-    palette: {
-        primary1Color: grey500,
-        primary2Color: blueGrey900,
-        primary3Color: '#b0ac9a',
-        pickerHeaderColor: '#b0ac9a',
-    }
+  palette: {
+    primary1Color: grey500,
+    primary2Color: blueGrey900,
+    primary3Color: '#b0ac9a',
+    pickerHeaderColor: '#b0ac9a',
+  }
 };
+
 
 const theme = getMuiTheme(customTheme);
 
 export default class App extends React.Component {
 
-    render() {
-        return (
-            <div>
-                <MuiThemeProvider muiTheme={theme}>
-                    <MainTemplate />
-                </MuiThemeProvider>
-                <div id="footer">
-                    <div id="footerHeader">
-                        <TabbedDrawer labels={["Console", "Python"]} iconClass={["fa fa-terminal", "fa fa-flask"]}>
-                            <Console />
-                            <PythonConsole pythonNotebookPath={"http://" + window.location.hostname + ":" + window.location.port + "/notebooks/notebook.ipynb"} />
-                        </TabbedDrawer>
-                    </div>
-                </div>
-            </div>
-        )
+  render () {
+    return (
+      <div>
+        <MuiThemeProvider muiTheme={theme}>
+          <MainTemplate />
+        </MuiThemeProvider>
+        <div id="footer">
+          <div id="footerHeader">
+            <TabbedDrawer labels={["Console", "Python"]} iconClass={["fa fa-terminal", "fa fa-flask"]}>
+              <Console />
+              <PythonConsole pythonNotebookPath={"http://" + window.location.hostname + ":" + window.location.port + "/notebooks/notebook.ipynb"} />
+            </TabbedDrawer>
+          </div>
+        </div>
+      </div>
+    )
 
-    }
+  }
 
 }
--- Utils.js
+++ Utils.js
@@ -1,53 +1,53 @@
 import React from 'react';
 import {
-	execPythonMessage, 
-	evalPythonMessage
+  execPythonMessage, 
+  evalPythonMessage
 } from 'geppetto-client/js/communication/geppettoJupyter/GeppettoJupyterUtils';
 
 
 const Utils = {
 
-	getHTMLType: function (type) {
+  getHTMLType: function (type) {
 
-		let htmlType;
-		switch (type) {
-		case "int":
-			htmlType = "number";
-			break;
-		default:
-			htmlType = "text";
-			break;
-		}
-		return htmlType;
+    let htmlType;
+    switch (type) {
+    case "int":
+      htmlType = "number";
+      break;
+    default:
+      htmlType = "text";
+      break;
+    }
+    return htmlType;
 
-	},
-
-	isObject: function (item) {
-		return (item && typeof item === 'object' && !Array.isArray(item));
-	},
+  },
+ 
+  isObject: function (item) {
+    return (item && typeof item === 'object' && !Array.isArray(item));
+  },
     
-	// FIXME: Hack to remove scaped chars (\\ -> \ and \' -> ') manually
-	convertToJSON (data){
-		if (typeof data === 'string' || data instanceof String){
-			return JSON.parse(data.replace(/\\\\/g, '\\').replace(/\\'/g, '\''))
-		}
-		return data
-	},
+  // FIXME: Hack to remove scaped chars (\\ -> \ and \' -> ') manually
+  convertToJSON (data){
+    if (typeof data === 'string' || data instanceof String){
+      return JSON.parse(data.replace(/\\\\/g, '\\').replace(/\\'/g, '\''))
+    }
+    return data
+  },
 
-	getErrorResponse (data){
-		let parsedData = this.convertToJSON(data)
-		if (parsedData.hasOwnProperty("type") && parsedData['type'] == 'ERROR'){
-			return { 'message': parsedData['message'], 'details' : parsedData['details'] }
-		}
-		return null;
-	},
+  getErrorResponse (data){
+    let parsedData = this.convertToJSON(data)
+    if (parsedData.hasOwnProperty("type") && parsedData['type'] == 'ERROR'){
+      return { 'message': parsedData['message'], 'details' : parsedData['details'] }
+    }
+    return null;
+  },
 
-	parsePythonException (exception){
-		return <pre dangerouslySetInnerHTML={{ __html: IPython.utils.fixConsole(exception) }} />
-	},
+  parsePythonException (exception){
+    return <pre dangerouslySetInnerHTML={{ __html: IPython.utils.fixConsole(exception) }} />
+  },
 
-	execPythonMessage,
-	evalPythonMessage
+  execPythonMessage,
+  evalPythonMessage
 }
 
 export default Utils
