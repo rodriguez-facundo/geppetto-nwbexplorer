7195281c67ff94566eb77ab4bbd0345de60c2736
Filippo Ledda <filippo@metacell.us>
2019-03-25 17:06:34 +0100

    lazy loading backend implementation

--- components/configuration/controlPanelConfiguration.js
+++ components/configuration/controlPanelConfiguration.js
@@ -17,39 +17,6 @@ let controlPanelColMeta = [
   }
 ];
 
-/**
- * Operates on an instance of a state variable and plots in accordance
- */
-function clickAction ($instance$) {
-  let instanceX = Instances.getInstance($instance$.getPath());
-  let instanceX_values = instanceX.getVariable().getWrappedObj().initialValues;
-  if (typeof instanceX_values[0] !== 'undefined') {
-    if (instanceX_values[0].value.eClass === 'MDTimeSeries') {
-      if (typeof instanceX_values[0].value.value[0] !== 'undefined') {
-        if (instanceX_values[0].value.value[0].eClass === 'Image') {
-          G.addWidget('CAROUSEL', {
-            files: ['data:image/png;base64,' + instanceX_values[0].value.value[0].data],
-            onClick: function () {
-              return 0;
-            },
-            onMouseEnter: function () {
-              return 0;
-            },
-            onMouseLeave: function () {
-              return 0;
-            },
-          });
-        }
-      }
-    } else {
-      G.addWidget(0).then(w => {
-        w.plotXYData(Instances.getInstance($instance$.getPath()), Instances.getInstance($instance$.getPath().split('.')[0] + '.time')).setPosition(130, 35).setName($instance$.getPath());
-      });
-    }
-  }
-}
-window.controlPanelClickAction = clickAction; // we don't like global variables but we like less putting the code in a string 
-
 let controlPanelConfig = {
   "VisualCapability": {},
   "Common": {
